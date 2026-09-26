9e3a0413589d6bfc3bddebfc2de42b25b2a2e4a6
Aaronica123 <k.aaronmutua@gmail.com>
2026-09-26 16:15:58 +0300

    house register api implementation

--- backend/files/actions/house.js
+++ backend/files/actions/house.js
@@ -5,15 +5,16 @@ export default async function House(req,res){
     try{
     const{body}=req.body;
     console.log(JSON.parse(body));
+    const{house_coordinates}=JSON.parse(body);
     const{house_location}=JSON.parse(body);
     var files=null;
-    if(house_location.lat>90||house_location.lat<-90||house_location.long>180||house_location.long<-180){
+    if(house_coordinates.lat>90||house_coordinates.lat<-90||house_coordinates.long>180||house_coordinates.long<-180){
         return res.status(409).json({message:"check coordinates"});
     }else{
-        const{house_name,id}=JSON.parse(body);
+        const{house_available,house_name,house_rooms,house_type,user_id}=JSON.parse(body);
         const degrees={
-            lat:house_location.lat,
-            long:house_location.long
+            lat:house_coordinates.lat,
+            long:house_coordinates.long
         };
         const result=await min_client.bucketExists("mmustimages");
         if(!result){
@@ -21,9 +22,9 @@ export default async function House(req,res){
         }else{
             console.log("bucket already exists");
         }
-        console.log(JSON.stringify(house_location).length);
+        console.log(JSON.stringify(house_coordinates).length);
         console.log(req.files,req.file);
-        // const buffer=Buffer.alloc(JSON.stringify(house_location).length,JSON.stringify(house_location));
+        // const buffer=Buffer.alloc(JSON.stringify(house_coordinates).length,JSON.stringify(house_coordinates));
         if(req.file){
             files=req.file;
         }else if(req.files){
@@ -33,15 +34,30 @@ export default async function House(req,res){
         for(const data of files){
             const stream=fs.createReadStream(`${data.path}`);
             await min_client.putObject('mmustimages',
-                `${id}/${house_name}/${degrees.lat}/${degrees.long}/${data.filename}`,
+                `${user_id}/${house_name}/${degrees.lat}/${degrees.long}/${data.filename}`,
             stream,data.size).then(()=>{
                 fs.unlink(`${data.path}`,()=>{});
             })
             
             console.log("Image has been successfully uploaded");
         }
-        }else{console.log("Files are null");}
-        return res.status(200).json({message:"Connection to minio successful"});
+        }else{console.log("Files are null");
+
+        }
+        await client.schema("mmust_homes").from("houses").insert({
+            user_id:Number(user_id),
+            house_name:house_name,
+            house_location:house_location,
+            house_type:house_type,
+            house_rooms:Number(house_rooms),
+            house_coordinates:degrees,
+            house_available:Number(house_available)
+            
+        }).then(()=>{
+            console.log("house created and stored");
+            return res.status(200).json({message:"House added"});
+        });
+        
     }
     }catch(error){
         console.log(error);
